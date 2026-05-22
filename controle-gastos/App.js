32d5f58e313057be59f19c8ec2eda1fb48043f99
Matheus Coronado <matheuscoronado48@gmail.com>
2026-05-21 21:49:46 -0300

    Corrige o caminho de importação do componente HomeScreen

--- controle-gastos/App.js
+++ controle-gastos/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import HomeScreen from '.components/ExpenseItem-styled-edit';
+import HomeScreen from './components/ExpenseItem-styled-edit';
 
 export default function App() {
  return <HomeScreen/> 
